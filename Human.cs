62c1e1942ab00a237efef9c9e18da7fe677081ca
denis <106389581+fishman123456@users.noreply.github.com>
2022-11-29 12:20:01 +0700

    Initial commit

---
+++ Human.cs
@@ -0,0 +1,22 @@
+﻿
+
+    internal class Human
+    {
+        public string _name { get; set; }
+        public string _surname { get; set; }
+        public int _age { get; set; }
+        public Human() { }
+        public Human(string name, string surname, int age)
+        {
+            _name = name;
+            _surname = surname;
+            _age = age;
+        }
+    public override string ToString()
+    {
+        return $"\n Имя: {_name}  фамилия: {_surname} возраст: {_age}\n";
+    }
+}
+
+
+
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+Human one = new Human("Иринка", "Заречнева", 35);
+Human two = new Human("Ленчик", "Шалаева", 30);
+Human three = new Human("Ольга", "Минина", 32);
+Human four = new Human("Катя", "Усикова", 31);
+Human five = new Human("Ольга", "Еременко", 42);
+Human six = new Human("Катя", "Солдатова", 40);
+
+Human seven = new Human("Юльчик", "Иванова", 35);
+List<Human> list = new List<Human>()
+{
+    one, two, three, four,
+    five, seven
+};
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
