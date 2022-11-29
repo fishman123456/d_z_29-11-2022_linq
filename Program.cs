3fa77a8f40fcb2a53f7994c5e2a071d73c6cb272
denis <106389581+fishman123456@users.noreply.github.com>
2022-11-29 13:01:59 +0700

    готово
    не просто все

--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ Human three = new Human("Ольга", "Минина", 32);
 Human four = new Human("Катя", "Усикова", 31);
 Human five = new Human("Ольга", "Еременко", 42);
 Human six = new Human("Катя", "Солдатова", 40);
-
 Human seven = new Human("Юльчик", "Иванова", 35);
 List<Human> list = new List<Human>()
 {
@@ -16,4 +15,14 @@ List<Human> list = new List<Human>()
 foreach (var item in list)
 {
     Console.WriteLine(item);
+}
+Console.WriteLine("Введите возраст:");
+int year=Convert.ToInt32( Console.ReadLine());
+
+List<Human> FoundYear = list.FindAll(item => item._age <= year);
+List<Human> Age_Year = new List<Human>();
+
+foreach (var item in FoundYear)
+{ 
+    Console.WriteLine($"Возраст {item._age} совпадает с Имя: {item._name}\t\t Фамилия: {item._surname}") ; 
 }
